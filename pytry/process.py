13ea2c17d2558c35bfac98dba25016fa899dcfde
Jan Ouwens <jan.ouwens@gmail.com>
2019-12-20 15:26:27 +0100

    Initial implementation

--- pytry/process.py
+++ pytry/process.py
@@ -1,3 +1,24 @@
+import json
 
 def process():
-    print("Jan yeah")
+    with open("/Users/jqno/Desktop/pinboard_export.json", "r") as in_file, \
+         open("/Users/jqno/Desktop/results.md", "w") as out_file:
+        decoded = json.load(in_file)
+        for bookmark in decoded:
+            if bookmark["description"] != "Twitter":
+                out_file.write(format(bookmark))
+
+def format(bookmark):
+    return (f"# {bookmark['description']}\n"
+            f"{bookmark['href']}\n"
+            f"{bookmark['time']}\n"
+            f"{format_tags(bookmark['tags'])}\n\n")
+
+def format_tags(tags):
+    hashtags = []
+    for tag in tags.split():
+        hashtags.append(f"#{tag}")
+    result = " ".join(hashtags)
+    if len(result) == 0:
+        return ""
+    return result + "\n"
