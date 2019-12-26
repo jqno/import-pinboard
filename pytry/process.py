3cc513727d2a695d787d17c0dd4259f9a612f7c5
Jan Ouwens <jan.ouwens@gmail.com>
2019-12-26 13:52:20 +0100

    Fixes whitespace

--- pytry/process.py
+++ pytry/process.py
@@ -1,5 +1,6 @@
 import json
 
+
 def process():
     with open("/Users/jqno/Desktop/pinboard_export.json", "r") as in_file, \
          open("/Users/jqno/Desktop/results.md", "w") as out_file:
@@ -8,11 +9,13 @@ def process():
             if bookmark["description"] != "Twitter":
                 out_file.write(format_bookmark(bookmark))
 
+
 def format_bookmark(bookmark):
     return (f"# {bookmark['description']}\n"
             f"{bookmark['href']}\n"
             f"{bookmark['time']}\n"
             f"{format_tags(bookmark['tags'])}\n\n")
+
 
 def format_tags(tags):
     hashtags = []
